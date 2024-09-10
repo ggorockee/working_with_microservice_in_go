6f1223e249fd93b1c7b1984171384dc83843718f
simplifywoopii <simplifywoopii@gmail.com>
2024-09-10 10:50:29 +0900

    Creating a Docker image and updating the Makefile

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gofiber/fiber/v2"
 	amqp "github.com/rabbitmq/amqp091-go"
+	"listener-service/event"
 	"log"
 	"math"
 	"os"
@@ -25,11 +25,23 @@ func main() {
 		log.Println(err)
 		os.Exit(1)
 	}
+	defer rabbitConn.Close()
+	log.Println("Connected to RabbitMQ!")
+
 	// start listening for messages
+	log.Println("Listening for and consuming RabbitMQ messages...")
 
 	// create consumer
+	consumer, err := event.NewConsumer(rabbitConn)
+	if err != nil {
+		panic(err)
+	}
 
 	// watch the queue and consume events
+	err = consumer.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
+	if err != nil {
+		log.Println(err)
+	}
 
 	app := Config{
 		Server: fiber.New(),
@@ -47,11 +59,12 @@ func connect() (*amqp.Connection, error) {
 
 	// don't continue until rabbit is ready
 	for {
-		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
 		if err != nil {
-			fmt.Println("RabbitMQ not yet ready...")
+			log.Println("RabbitMQ not yet ready...")
 			counts++
 		} else {
+			log.Println("Connected to RabbitMQ!")
 			connection = c
 			break
 		}
--- mail-service/go.mod
+++ mail-service/go.mod
@@ -3,10 +3,15 @@ module mail-service
 go 1.23.0
 
 require (
+	github.com/gofiber/fiber/v2 v2.52.5
+	github.com/vanng822/go-premailer v1.21.0
+	github.com/xhit/go-simple-mail/v2 v2.16.0
+)
+
+require (
 	github.com/PuerkitoBio/goquery v1.9.1 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
-	github.com/gofiber/fiber/v2 v2.52.5 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
@@ -19,8 +24,6 @@ require (
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/vanng822/css v1.0.1 // indirect
-	github.com/vanng822/go-premailer v1.21.0 // indirect
-	github.com/xhit/go-simple-mail/v2 v2.16.0 // indirect
 	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 )
